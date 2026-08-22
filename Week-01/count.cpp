829df9c3cfb440ff02dc701a11c42d843a3007ce
MD TASNIMUL HASAN <tasnimshaun3@gmail.com>
2026-08-22 23:29:32 +0800

    Add files via upload

---
+++ Week-01/count.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b;
+     cin >> a >> b;
+     int count =0;
+      for (int i = a;i<=b;i++)
+        {
+            if(i>=a || i<=b)
+            {
+                count++;
+            }
+        }
+        cout << count << endl;
+    return 0;
+}
