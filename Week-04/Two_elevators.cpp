aeec5da8193a133dfe5db412703d040a6a363b29
MD TASNIMUL HASAN <tasnimshaun3@gmail.com>
2026-09-06 21:38:50 +0800

    Add files via upload

---
+++ Week-04/Two_elevators.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+   int t;
+   cin >> t;
+   while(t--)
+   {
+     int a, b, c;
+     cin >> a >> b >> c;
+     int x = abs(a - 1);
+     int y;
+     if (b == 1)
+     {
+         y = 2 * abs(c - 1);
+     }  
+     else 
+     {
+        y = abs(b - c) + abs(c - 1);
+     }
+     if (x < y)
+     {
+         cout << 1 << endl;
+     }
+     else if (x > y)
+     {
+         cout << 2 << endl;
+     }
+     else 
+     {
+         cout << 3 << endl;
+     }
+   }
+   return 0;
+}
+// https://codeforces.com/problemset/problem/1729/A
