5f12c17b72ef38f0f338abc891cd89a9fc6aa539
MD TASNIMUL HASAN <tasnimshaun3@gmail.com>
2026-09-10 22:40:56 +0800

    Add files via upload

---
+++ Week-09/good.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+   int t;
+   cin >> t;
+   while(t--)
+   {
+       int n;
+       cin >> n;
+        bool good = true;
+    for (int i = 0; i < n; i++)
+     {
+        int score;
+        cin >> score;
+        if (score <= 4) 
+        {
+            good = false;
+        }
+    }
+        if(good)
+        {
+            cout << "YES" << endl;
+           
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+       }
+   
+   return 0;
+}
