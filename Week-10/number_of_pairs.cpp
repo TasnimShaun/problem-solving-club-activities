ed0a8b2b3d8f5e02101afc335d3cfb3a9d848bef
MD TASNIMUL HASAN <tasnimshaun3@gmail.com>
2026-09-13 22:27:09 +0800

    Add files via upload

---
+++ Week-10/number_of_pairs.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+   int t;
+   cin >> t;
+   while(t--)
+   {
+       int n;
+       long long l , r; 
+       cin >> n >> l >> r;
+       vector< long long > v(n);
+        for ( int i=0;i<n;i++)
+        {
+             cin  >> v[i];
+        }
+         
+        sort(v.begin(),v.end());
+         long long count =0;
+          
+          for( int i=0;i<n;i++)
+          {
+             int lft =  lower_bound(v.begin() + i+1, v.end(), l-v[i]) - v.begin();
+             int righ =  upper_bound(v.begin() + i+1, v.end(), r-v[i]) - v.begin();
+             count += (righ-lft);
+          }
+          cout << count << endl;
+   }
+   return 0;
+}
