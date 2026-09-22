e2207059ddbad396ae69eccc04a6718575c49ed6
MD TASNIMUL HASAN <tasnimshaun3@gmail.com>
2026-09-22 22:11:05 +0800

    Add files via upload

---
+++ Week-11/Practice-contest-06/A_Bone_Appetit.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+   int n,m;
+   cin >> n >> m;
+   int x, y;
+   cin >> x >> y;
+   int z = n*x + m*y;
+   cout << z << endl;
+
+    return 0;
+}
